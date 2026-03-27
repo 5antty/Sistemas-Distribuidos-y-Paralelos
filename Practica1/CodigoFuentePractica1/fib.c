04ce01906395b915e89ba8391f88835a77fd38e2
sxntty <sxntty@Novabook.168.0.38>
2026-03-27 13:35:55 -0300

    avance en prac1, falta terminar

--- Practica1/CodigoFuentePractica1/fib.c
+++ Practica1/CodigoFuentePractica1/fib.c
@@ -1,82 +1,98 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/time.h>
 #define repeticiones 10
 
-//Calcula Fibonacci recursivo
-unsigned long long fibRecursivo(unsigned long long  n);
-//Calcula Fibonacci iterativo
+// Calcula Fibonacci recursivo
+unsigned long long fibRecursivo(unsigned long long n);
+// Calcula Fibonacci iterativo
 unsigned long long fibIterativo(unsigned long long n);
-//Para calcular tiempo
+// Para calcular tiempo
 double dwalltime();
 
+int main(int argc, char *argv[])
+{
+  double timetick, timeend;
+  unsigned long long N;
+  unsigned long long resultado;
 
-int main(int argc, char *argv[]){
-double timetick, timeend;
-unsigned long long N;
-unsigned long long resultado;
+  // Chequeo de parametros
+  if ((argc < 2) || ((N = atoi(argv[1])) <= 0))
+  {
+    printf("\nError en los parametros. Usar: %s N \n", argv[0]);
+    exit(1);
+  }
 
-	// Chequeo de parametros
-	if ( (argc < 2) || ((N = atoi(argv[1])) <= 0)){
-		printf("\nError en los parametros. Usar: %s N \n", argv[0]);
-		exit(1);
-	}
-
-
-  //Fibonacci iterativo
-  printf("\n Calculando Fibonacci iterativo para N = %llu...\n",N);
+  // Fibonacci iterativo
+  printf("\n Calculando Fibonacci iterativo para N = %llu...\n", N);
   timetick = dwalltime();
-	for(int r=0;r<repeticiones;r++){
-		resultado = fibIterativo(N);
-	}
+  for (int r = 0; r < repeticiones; r++)
+  {
+    resultado = fibIterativo(N);
+  }
   timeend = dwalltime();
-  printf(" Resultado = %llu\n",resultado); 
-  printf(" Tiempo en segundos: %.10lf \n", (timeend - timetick)/repeticiones);
-  
-  //Fibonacci recursivo
-  printf("\n Calculando Fibonacci recursivo para N = %llu...\n",N);
+  printf(" Resultado = %llu\n", resultado);
+  printf(" Tiempo en segundos: %.10lf \n", (timeend - timetick) / repeticiones);
+
+  // Fibonacci recursivo
+  printf("\n Calculando Fibonacci recursivo para N = %llu...\n", N);
   timetick = dwalltime();
-	for(int r=0;r<repeticiones;r++){
-		resultado = fibRecursivo(N); 
-	}
+  for (int r = 0; r < repeticiones; r++)
+  {
+    resultado = fibRecursivo(N);
+  }
   timeend = dwalltime();
-  printf(" Resultado = %llu\n",resultado); 
-  printf(" Tiempo en segundos: %.10lf \n", (timeend - timetick)/repeticiones);
+  printf(" Resultado = %llu\n", resultado);
+  printf(" Tiempo en segundos: %.10lf \n", (timeend - timetick) / repeticiones);
 
-	
-  return(0);
-
+  return (0);
 }
 
 //--------------------------------------
 
-unsigned long long fibRecursivo(unsigned long long  n){
-  if (n <= 2){
-   return 1;
-  }else{
-   return fibRecursivo(n-1) + fibRecursivo(n - 2);
+unsigned long long fibRecursivo(unsigned long long n)
+{
+  if (n <= 2)
+  {
+    return 1;
+  }
+  else
+  {
+    return fibRecursivo(n - 1) + fibRecursivo(n - 2);
   }
 }
 
-unsigned long long fibIterativo(unsigned long long n){
-unsigned long long j = 0;
-unsigned long long i = 1;
-unsigned long long k;
-unsigned long long t;
+unsigned long long fibIterativo(unsigned long long n)
+{
+  unsigned long long j = 0;
+  unsigned long long i = 1;
+  unsigned long long k;
+  unsigned long long t;
 
-	for(k=1;k<=n;k++){
-		t = i + j;
-		i = j;
-		j = t;
-	}
- 	return j;
+  for (k = 1; k <= n; k++)
+  {
+    t = i + j;
+    i = j;
+    j = t;
+  }
+  return j;
 }
 
-double dwalltime(){
-        double sec;
-        struct timeval tv;
+double dwalltime()
+{
+  double sec;
+  struct timeval tv;
 
-        gettimeofday(&tv,NULL);
-        sec = tv.tv_sec + tv.tv_usec/1000000.0;
-        return sec;
+  gettimeofday(&tv, NULL);
+  sec = tv.tv_sec + tv.tv_usec / 1000000.0;
+  return sec;
 }
+
+/*
+  El recursivo tarda mas que el iterativo ya que tiene que ir cargando las
+  llamadas a las siguientes funciones al stack, mientras que el iterativo
+  solo va guardando los ultimos 2 resultados y va actualizando esos valores,
+  por lo que el iterativo tiene una complejidad de O(n) y el recursivo tiene
+  una complejidad de O(2^n) debido a la cantidad de llamadas recursivas que
+  se hacen.
+*/
--- Practica1/CodigoFuentePractica1/modulo.c
+++ Practica1/CodigoFuentePractica1/modulo.c
@@ -1,77 +1,95 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <sys/time.h>
 
-//Funcion para calcular el tiempo de ejecucion
+// Funcion para calcular el tiempo de ejecucion
 double dwalltime(void);
 
-int main(int argc, char* argv[]){
-	
-  	// Chequeo de parámetros. Se recibe N como longitud de los arreglos y m que es un divisor potencia de 2
-    if (argc != 3) {
-        printf("Uso: %s N m\n", argv[0]);
-        return 1;
-    }
-	
+int main(int argc, char *argv[])
+{
+
+	// Chequeo de parámetros. Se recibe N como longitud de los arreglos y m que es un divisor potencia de 2
+	if (argc != 3)
+	{
+		printf("Uso: %s N m\n", argv[0]);
+		return 1;
+	}
+
 	unsigned long N = atol(argv[1]);
 	unsigned long m = atol(argv[2]);
-	
+
 	// Chequea si m es potencia de 2
-	if (m <= 0 || (m & (m - 1)) != 0) {
+	if (m <= 0 || (m & (m - 1)) != 0)
+	{
 		printf("m no es potencia de 2.\n");
 		return 1;
 	}
-	
-	unsigned long* a;
-	unsigned long* resultModulo;
-	unsigned long* resultOpt;
-	
+
+	unsigned long *a;
+	unsigned long *resultModulo;
+	unsigned long *resultOpt;
+
 	// Aloca memoria
-	a = (unsigned long*)malloc(sizeof(unsigned long)*N);
-	resultModulo = (unsigned long*)malloc(sizeof(unsigned long)*N);
-	resultOpt = (unsigned long*)malloc(sizeof(unsigned long)*N);
+	a = (unsigned long *)malloc(sizeof(unsigned long) * N);
+	resultModulo = (unsigned long *)malloc(sizeof(unsigned long) * N);
+	resultOpt = (unsigned long *)malloc(sizeof(unsigned long) * N);
 	unsigned long i;
-		
-	//Inicializa el arreglo a	
-	for(i=0;i<N;i++){
-		a[i]=i;
+
+	// Inicializa el arreglo a
+	for (i = 0; i < N; i++)
+	{
+		a[i] = i;
 	}
 
-	//Calculo usando %
+	// Calculo usando %
 	double timetick = dwalltime();
-	for(i=0;i<N;i++){
-		resultModulo[i] = i%m;
+	for (i = 0; i < N; i++)
+	{
+		resultModulo[i] = i % m;
 	}
 	printf("Tiempo usando %%: %f \n", dwalltime() - timetick);
-	 
-	//Calculo usando la equivalencia
+
+	// Calculo usando la equivalencia
 	timetick = dwalltime();
-	for(i=0;i<N;i++){
-		resultOpt[i] = i&(m-1);
+	for (i = 0; i < N; i++)
+	{
+		resultOpt[i] = i & (m - 1);
 	}
 	printf("Tiempo usando equivalencia: %f \n", dwalltime() - timetick);
-	
-	//Validando resultados
-	for(i=0;i<N;i++){
-		if(resultModulo[i]!=resultOpt[i]) printf("Error\n");
+
+	// Validando resultados
+	for (i = 0; i < N; i++)
+	{
+		if (resultModulo[i] != resultOpt[i])
+			printf("Error\n");
 	}
-	
+
 	// Libera memoria
 	free(a);
 	free(resultModulo);
 	free(resultOpt);
-	
+
 	return 0;
 }
 
 //---------------------------------------------
 
-double dwalltime(){
+double dwalltime()
+{
 	double sec;
 	struct timeval tv;
 
-	gettimeofday(&tv,NULL);
-	sec = tv.tv_sec + tv.tv_usec/1000000.0;
+	gettimeofday(&tv, NULL);
+	sec = tv.tv_sec + tv.tv_usec / 1000000.0;
 	return sec;
 }
+
+/*
+	La optimizacion consiste en que debido a que los elementos del arreglo
+	son nuemeros desde 0 hasta N-1, el resultado de la operacion modulo m
+	se puede obtener utilizando la equivalencia i % m == i & (m - 1)
+	cuando m es potencia de 2 ya que se realiza el and bit a bit y si
+	comṕaro el valor de i con (m-1), donde m es el parametro recibido en la
+	ejecucion (multiplo de 2) me devuelve el modulo.
+*/
