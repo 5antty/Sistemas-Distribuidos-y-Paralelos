a1744050bdb20e4b6b4a115e216de5faa16b147e
sxntty <sxntty@Novabook.168.0.38>
2026-04-01 11:17:25 -0300

    hasta ej de overhead

--- Practica1/CodigoFuentePractica1/Gauss.c
+++ Practica1/CodigoFuentePractica1/Gauss.c
@@ -1,28 +1,41 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char*argv[]){
-	
+int main(int argc, char *argv[])
+{
+
 	// Chequeo de parámetros. Se recibe N que es el número hasta el cual se quiere calcular la suma de los números naturales.
-    if (argc != 2) {
-        printf("Uso: %s N\n", argv[0]);
-        return 1;
-    }
-	
+	if (argc != 2)
+	{
+		printf("Uso: %s N\n", argv[0]);
+		return 1;
+	}
+
 	unsigned long N = atol(argv[1]);
 	unsigned long sum = 0;
 	int i;
 
 	// Calcula la sumatoria de los N numeros consecutivos
-	for(i=1;i<=N;i++){
-		sum+=i;
+	for (i = 1; i <= N; i++)
+	{
+		sum += i;
 	}
 
-	// Compara la suma anterior con la suma de Gauss, deben ser iguales	
-	if( sum == ((N*(N+1))/2) ){
+	// Compara la suma anterior con la suma de Gauss, deben ser iguales
+	if (sum == ((N * (N + 1)) / 2))
+	{
 		printf("Resultado Correcto\n");
-	}else{
+	}
+	else
+	{
 		printf("ERROR\n");
 	}
-		
 }
+
+/*
+	Es incorrecta porque el resultado de la suma de los N números naturales puede
+	exceder el rango de un entero sin signo (unsigned long) para valores grandes de N,
+	lo que provoca un desbordamiento y un resultado incorrecto.
+	Para evitar esto, se debería utilizar un tipo de dato más grande, como unsigned
+	long long, o implementar una función que calcule la suma sin riesgo de desbordamiento.
+*/
--- Practica1/CodigoFuentePractica1/direccionamiento.c
+++ Practica1/CodigoFuentePractica1/direccionamiento.c
@@ -1,20 +1,22 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <sys/time.h>
 
-//Funcion para calcular el tiempo de ejecucion
+// Funcion para calcular el tiempo de ejecucion
 double dwalltime(void);
 
-int main(int argc, char*argv[]){
- double timetick;
- 
- 	// Chequeo de parámetros. Se recibe N como longitud del arreglo y el numero de repeticiones en que se realiza el calculo
-    if (argc != 3) {
-        printf("Uso: %s N Repeticiones\n", argv[0]);
-        return 1;
-    }
-	
+int main(int argc, char *argv[])
+{
+	double timetick;
+
+	// Chequeo de parámetros. Se recibe N como longitud del arreglo y el numero de repeticiones en que se realiza el calculo
+	if (argc != 3)
+	{
+		printf("Uso: %s N Repeticiones\n", argv[0]);
+		return 1;
+	}
+
 	unsigned long N = atol(argv[1]);
 	unsigned long R = atol(argv[2]);
 	unsigned long r;
@@ -22,12 +24,14 @@ int main(int argc, char*argv[]){
 	unsigned long *a;
 
 	// Aloca memoria para el arreglo
-	a = (unsigned long*)malloc(sizeof(unsigned long)*N);
+	a = (unsigned long *)malloc(sizeof(unsigned long) * N);
 
 	// Inicializa el arreglo con direccionamiento tradicional a[i]
 	timetick = dwalltime();
-	for(r=0;r<R;r++){
-		for(i=0;i<N;i++){
+	for (r = 0; r < R; r++)
+	{
+		for (i = 0; i < N; i++)
+		{
 			a[i] = 1;
 		}
 	}
@@ -36,26 +40,34 @@ int main(int argc, char*argv[]){
 	// Inicializa el arreglo con direccionamiento a partir de un puntero p
 	timetick = dwalltime();
 	register unsigned long *p;
-	for(r=0;r<R;r++){
-		for(p=a ; p < a+N ; p++){
+	for (r = 0; r < R; r++)
+	{
+		for (p = a; p < a + N; p++)
+		{
 			*p = 1;
 		}
 	}
 	printf("Tiempo de direccionamiento *p: %f \n", dwalltime() - timetick);
 
-	//Libera memoria
+	// Libera memoria
 	free(a);
- 
+
 	return 0;
 }
 
 //----------------------------
 
-double dwalltime(){
+double dwalltime()
+{
 	double sec;
 	struct timeval tv;
 
-	gettimeofday(&tv,NULL);
-	sec = tv.tv_sec + tv.tv_usec/1000000.0;
+	gettimeofday(&tv, NULL);
+	sec = tv.tv_sec + tv.tv_usec / 1000000.0;
 	return sec;
 }
+
+// La estrategia mas rapida es la de direccionamiento a partir de un puntero,
+//  ya que se evita el calculo del indice en cada iteracion, pues con el puntero
+//  solo va a una posicion y luego se va corriendo a partir de la misma,
+//  lo que mejora el rendimiento del programa.
